8305d09160d82eda91b9a97ef45ce70691c1a38d
Amit Verma <144882964+AmitVerma-01@users.noreply.github.com>
2026-07-25 17:48:26 +0530

    python.day2

---
+++ firstprogram.py
@@ -0,0 +1,62 @@
+name = "Ankur" 
+age = 18 
+price = 20.99
+
+ 
+print(type(name))
+print(type(age))
+print(type(price))
+
+a = 4 
+b = 2 
+diff = a - b
+print(diff)
+ 
+
+ #arithmetic operators
+
+a = 10 
+b = 5
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b)
+print(a ** b)
+
+
+# relational operators
+
+a = 40
+b = 10
+
+
+print(a == b)  # false
+print(a != b)  # true
+print(a >= b)  # true
+print(a > b)   # true
+print(a <= b)  # false
+print(a < b)   # false
+ 
+
+# assignment operators 
+num = 10 
+
+num += 5 
+print("num : ", num)
+
+ #logical operators
+
+a = 10 
+b = 20
+print(not False)
+print(not (a < b))
+
+val1 = False
+val2 = True
+
+print("AND operator: ", val1 and val2)
+
+
+print("OR operator: ", val1 or val2)
